af1d6ee3e8bd622abdd6244e06c12ab22874cbb5
Jeffrey Espiritu <jeff_pro3@hotmail.com>
2014-07-17 00:47:46 -0400

    Added more fields to register table to temporarily store applications before inserting new accounts into login. Also includes upgrade_xxxx.sql file

--- sql-files/register.sql
+++ sql-files/register.sql
@@ -1,7 +1,14 @@
 CREATE TABLE IF NOT EXISTS `register` (
   `id` int(11) unsigned NOT NULL auto_increment,
-  `account_id` int(11) unsigned NOT NULL default '0',
-  `ip` varchar(20) NOT NULL default '',
+  `time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `userid` varchar(23) NOT NULL default '',
+  `user_pass` varchar(32) NOT NULL default '',
+  `sex` enum('M','F','S') NOT NULL default 'M',
+  `email` varchar(39) NOT NULL default '',
+  `level` tinyint(3) NOT NULL default '0',
+  `birthdate` DATE NOT NULL DEFAULT '0000-00-00',
+  `ip` varchar(100) NOT NULL default '',
   `data` text,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `name` (`userid`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1;
